7d2275fd9cf0a5185b7b9a361ce7e74c32d346c4
kennethprinceagbodjan <kennethprinceagbodjan@gmail.com>
2024-05-20 12:47:04 +0000

    fix: replace navigate method with router.push

--- components/project-card.tsx
+++ components/project-card.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import Image from 'next/image'
 import { Launch } from '@carbon/icons-react'
+import { useRouter } from 'next/router';
+
+const router = useRouter();
 
 type Props = {}
 
@@ -14,7 +17,7 @@ const ProjectCard = (props: Props) => {
         {/* img overlay */}
         <div className="h-[280px] group absolute top-0 left-0 w-full px-4 pt-4 rounded-t-2xl flex justify-center border-b border-neutral-600 hover:backdrop-blur-sm hover:bg-neutral-50/50 dark:hover:bg-neutral-900/50 transition-all ">
             <div className="flex items-center justify-center opacity-0 group-hover:opacity-100 transition-all " >
-                <Launch size={32} className="cursor-pointer dark:text-neutral-200" onClick={() => navigate("http://mern-stack-memories.vercel.app/")} />
+                <Launch size={32} className="cursor-pointer dark:text-neutral-200" onClick={() => router.push("http://mern-stack-memories.vercel.app/")} />
                 {/* <h5 className="text-white" >{moment(post.createdAt).fromNow()}</h5> */}
             </div>
             {/* go-to icon here */}
